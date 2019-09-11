91003d2e98e276721996d099cda6a28e7e2763c1
gengjiaan <gengjiaan@360.cn>
2019-09-11 17:07:47 +0800

    [CORE] Replace prefix of artifactId from spark to xsql. #20

--- build-plugin.sh
+++ build-plugin.sh
@@ -9,7 +9,7 @@ SPARK_VERSION=$(mvn help:evaluate -Dexpression=spark.version $@ 2>/dev/null\
     | grep -v "INFO"\
     | grep -v "WARNING"\
     | tail -n 1)
-TGZ_NAME=spark-assembly_2.11-$VERSION-dist.tgz
+TGZ_NAME=xsql-assembly_2.11-$VERSION-dist.tgz
 FINAL_NAME=xsql-$VERSION-plugin-spark-$SPARK_VERSION.tgz
 cd assembly/target
 mv $TGZ_NAME ../../$FINAL_NAME
